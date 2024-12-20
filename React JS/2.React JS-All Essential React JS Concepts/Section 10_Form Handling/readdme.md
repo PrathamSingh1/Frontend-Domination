66e7753aa66bf48360bee866bc8ad433755fca28
Pratham Singh <prathamjii36@gmail.com>
2024-12-21 05:05:09 +0530

    Form Handling

--- React JS/2.React JS-All Essential React JS Concepts/Section 10_Form Handling/readdme.md
+++ React JS/2.React JS-All Essential React JS Concepts/Section 10_Form Handling/readdme.md
@@ -3,3 +3,20 @@ form handling - form submission par website reload ho jaati hai aur react ka mai
 useRef
 controlled components
 react hook form
+
+
+useRef = is tareeke mein hum har input ko select kar lete hain and unki value tab nikaalte hai jab form submit hota hai 
+
+// useref kisi bhi html input ko select kar skte ho
+
+
+
+controlled components = 
+
+// aap jabhi kuchh likhen useState ke through data real time par update kar dein
+// jaise hi kuchh type ho set state kardo nayi value ke barabar
+
+
+react hook form = 
+
+// It's a package 
--- React JS/2.React JS-All Essential React JS Concepts/Section 10_Form Handling/src/App.jsx
+++ React JS/2.React JS-All Essential React JS Concepts/Section 10_Form Handling/src/App.jsx
@@ -1,14 +1,168 @@
-import React from 'react'
 
-const App = () => {
-  return (
-    <div className='p-4'>
-      <form action="">
-        <input className='border-2' type="text" placeholder='name' />
-        <input type="submit" />
-      </form>
-    </div>
-  )
-}
+//       Form handling - react hook form
 
-export default App
+
+
+
+
+// import React from 'react';
+// import { useForm } from 'react-hook-form'
+
+// const App = () => {
+//   const {register, handleSubmit} = useForm();
+//   console.log(register());
+//   return (
+//     <div>
+//       <form action="" onSubmit={handleSubmit(data => console.log(data))}>
+//         <input {...register('name')} type="text" placeholder='name' />
+//         <input {...register('email')} type="email" placeholder='email' />
+//         <input type="submit" />
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         Form handling by - controlled components
+
+
+
+
+
+// import React from 'react';
+// import { useState } from 'react';
+
+// const App = () => {
+//   const [val, setVal] = useState({name: "", email: ""})
+
+//   const handleClick = (event)=>{
+//     const realVal = setVal({...val, name: event.target.value}) 
+//     const realVal1 = setVal({...val, email: event.target.value}) 
+//   }
+
+
+//   const handleSubmit = (event)=>{
+//     event.preventDefault();
+//     console.log(val);
+
+//   }
+
+//   return (
+//     <div>
+//       <form action="" onSubmit={handleSubmit} className='p-5'>
+//         <input onChange={handleClick} type="text" placeholder='name' />
+//         <input onChange={handleClick} type="email" placeholder='email' />
+//         <input type="submit" />
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    Form handling by useRef
+
+
+
+
+// import React from 'react';
+// import { useRef } from 'react';
+
+// const App = () => {
+//   const name = useRef(null);
+//   const age = useRef(null);
+
+//   const handleSubmit = (event)=>{
+//     event.preventDefault();
+//     console.log(name.current.value);
+//     console.log(age.current.value);
+//   }
+
+//   return (
+//    <form action="" onSubmit={handleSubmit} className='p-4 border-2'>
+//     <input ref={name} placeholder='name' type="text"  />
+//     <input ref={age} placeholder='age' type="text"  />
+//     <input type="submit" />
+//    </form>
+//   )
+// }
+
+// export default App
