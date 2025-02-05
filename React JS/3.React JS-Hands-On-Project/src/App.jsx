511940a8a41b203ab5db99567c28138a82dd589c
Pratham Singh <prathamjii36@gmail.com>
2025-02-05 05:11:57 +0000

    project

--- React JS/3.React JS-Hands-On-Project/src/App.jsx
+++ React JS/3.React JS-Hands-On-Project/src/App.jsx
@@ -3,10 +3,11 @@ import React from "react";
 const App = () => {
   return (
     <div className="h-screen w-screen bg-red-100">
-      <nav>
+      <nav className="w-[15%] h-full bg-zinc-100">
         <a href="/create">Add new Product</a>
         <h1>Category</h1>
         <ul>
+          <li>cat 1</li>
           <li>cat 1</li>
         </ul>
       </nav>
