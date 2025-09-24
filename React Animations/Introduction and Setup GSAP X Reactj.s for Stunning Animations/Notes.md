c8aa3f430d0b9dc1b537fa339e0f6c957fe61d59
PrathamSingh1 <prathamjii36@gmail.com>
2025-09-24 21:16:05 +0000

    React Animation

---
+++ React Animations/Introduction and Setup GSAP X Reactj.s for Stunning Animations/Notes.md
@@ -0,0 +1,3 @@
+Step 1 - Create Vite app
+step 2-  npm i gsap
+stem 3 - npm i @gsap/react
--- React Animations/Introduction and Setup GSAP X Reactj.s for Stunning Animations/src/App.jsx
+++ React Animations/Introduction and Setup GSAP X Reactj.s for Stunning Animations/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef } from 'react'
 
 const App = () => {
+  const boxRef = useRef();
+
+  useGSAP(() => {
+    gsap.to(boxRef.current, {
+      x: 600,
+      y: 300,
+      duration: 5
+    })
+  })
+  
   return (
-    <div>App</div>
+    <div>
+      <div ref={boxRef} className="box">
+
+      </div>
+    </div>
   )
 }
 
